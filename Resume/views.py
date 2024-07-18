c622b626933d7dc4eb5ce7f9e52ba7bed196984c
srikanth <banothusrikanth267@gmail.com>
2024-07-18 23:48:43 +0530

    added

--- Resume/views.py
+++ Resume/views.py
@@ -5,38 +5,25 @@ def overview(request):
     details=Detail.objects.all()
     return render(request,"index.html",{"details": details })
 
-
 def education(request):
     details=Detail.objects.all()
     return render(request, "education.html",{"details": details })
-
-
-
 
 def Skills(request):
     details=Detail.objects.all()
     return render(request, "skills.html",{"details": details })
 
-
-
-
-
 def Projects(request):
     details=Detail.objects.all()
     return render(request, "projects.html",{"details": details })
-
-
-
 
 def Uiapplications(request):
     details=Detail.objects.all()
     return render(request, "uiapplications.html",{"details": details })
 
-
 def Workexperience(request):
     details=Detail.objects.all()
     return render(request, "work_experience.html",{"details": details })
-
 
 def Contact(request):
     details=Detail.objects.all()
